56aef2c6a136d3ddfc689423b2109ab1716a0e0b
caioddm <caioduarte.diniz@gmail.com>
2016-10-07 19:52:30 -0500

    Problem 2 done

--- hw2/Problem2/problem2.m
+++ hw2/Problem2/problem2.m
@@ -15,20 +15,31 @@ covmatrix = cov(data);
 [eigenvectors, eigenvalues] = svd(covmatrix);
 
 %(b) plot of the eigenvalues sorted in descending order
+figure('Position', [100 100 1024 800]);
+
 eigenvalues = diag(eigenvalues);
 sumeig = sum(eigenvalues);
 totaleig = 0;
 eigplot = [];
 for idx = 1 : 400
-    totaleig = totaleig + eigenvalues(idx);
-    eigplot = [eigplot; idx totaleig/sumeig];
+    totaleig = totaleig + eigenvalues(idx);    
+    eigplot = [eigplot; log10(idx) eigenvalues(idx) totaleig/sumeig];
 end
-figure;
+subplot(2, 1, 1);
 plot(eigplot(:, 1), eigplot(:, 2));
+ylabel('Eigenvalue');
+xlabel('Component (log scaled)');
+title('Eigenvalues of each component');
+
+subplot(2, 1, 2);
+plot(eigplot(:, 1), eigplot(:, 3));
+ylabel('Percentage of variance represented');
+xlabel('Components (log scaled)');
+title('Amount of data variance captured with the first N components ');
 
 %(c) scatter plot from the first three dimensions of PCA
 figure;
-pcavectors = eigenvectors(:, 1:3);
+pcavectors = data * eigenvectors(:, 1:3);
 scatter3(pcavectors(:, 1), pcavectors(:, 2), pcavectors(:, 3))
 
 
